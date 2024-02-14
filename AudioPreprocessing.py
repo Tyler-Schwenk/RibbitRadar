24b2f37e1860c4fca3a0275353ed41927da40bcb
Tyler Schwenk <101892991+Tyler-Schwenk@users.noreply.github.com>
2024-02-14 10:22:06 -0800

    Add files via upload

---
+++ AudioPreprocessing.py
@@ -0,0 +1,237 @@
+import torchaudio
+import math
+import os
+import re
+import shutil
+import GetFFMPEG
+from tkinter import messagebox
+from pydub import AudioSegment
+from pydub.utils import mediainfo 
+
+def split_all_audio_files(input_dir, output_dir, progress_callback, segment_length_sec=10, file_types=('.wav', '.WAV')):
+    """
+    Splits each audio file in the input directory into segments of specified length and saves them in the output directory.
+
+    Parameters:
+    input_dir (str): The directory containing the original audio files.
+    output_dir (str): The directory where the segmented audio files will be saved.
+    segment_length_sec (int, optional): Length of each audio segment in seconds. Defaults to 10 seconds.
+    file_types (tuple, optional): Tuple of file extensions to consider for processing. Defaults to ('.wav', '.WAV').
+
+    This function processes each audio file in the input directory that matches the specified file types. It splits the audio 
+    files into segments of a specified length and saves each segment as a new audio file in the output directory. 
+    Errors during processing are caught and displayed in a messagebox.
+
+    Returns:
+    None
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    num_files = len(input_dir)
+
+    for filename in os.listdir(input_dir):
+        if filename.endswith(file_types):
+            audio_path = os.path.join(input_dir, filename)
+            audio_path = os.path.normpath(audio_path)
+
+            try:
+                waveform, sample_rate = torchaudio.load(audio_path, format="wav")
+                num_samples_segment = segment_length_sec * sample_rate
+                total_segments = math.ceil(waveform.shape[1] / num_samples_segment)
+
+                for i in range(total_segments):
+                    start = i * num_samples_segment
+                    end = start + num_samples_segment
+                    segment = waveform[:, start:end]
+                    segment_filename = f"{filename.rstrip('.wav').rstrip('.WAV')}_segment{i}.wav"
+                    segment_path = os.path.join(output_dir, segment_filename)
+                    segment = (segment * 32767).short()  # Convert to 16-bit PCM format
+                    torchaudio.save(segment_path, segment, sample_rate)
+
+            except Exception as e:
+                messagebox.showerror('Error', f'Error processing file {filename}: {str(e)}')
+                
+
+def extract_metadata_from_files_in_directory(directory, progress_callback, file_types=('.wav', '.WAV')):
+  """
+    Extracts metadata from audio files in the specified directory.
+
+    Parameters:
+    directory (str): The directory containing audio files from which metadata is to be extracted.
+    file_types (tuple, optional): Tuple of file extensions to consider for metadata extraction. Defaults to ('.wav', '.WAV').
+
+    Returns:
+    dict: A dictionary where each key is a filename, and the value is another dictionary of extracted metadata.
+    """
+  metadata_dict = {}
+  ffmpeg_executable, _ = GetFFMPEG.get_ffmpeg_path()  # Retrieve the path to the local ffmpeg binary
+
+
+  for file in os.listdir(directory):
+    if file.endswith(file_types):
+      filepath = os.path.join(directory, file)
+      filepath = os.path.normpath(filepath)
+      metadata = mediainfo(filepath)
+
+      # Extract desired information from the comment
+      comment = metadata.get('TAG', {}).get('comment', '')
+      match = re.search(r"Recorded at (.*) by (AudioMoth .*) at .* while .* and temperature was (.*C)\.", comment)
+
+      # Additional check for no match found
+      if match:
+        recorded_at, device_id, temperature = match.groups()
+
+        # Create a new dictionary with only the desired information
+        simplified_metadata = {
+          'filename': file,
+          'recorded_at': recorded_at,
+          'device_id': device_id,
+          'temperature': temperature,
+        }
+        metadata_dict[file] = simplified_metadata  # Store metadata using filename as key
+      else:
+        progress_callback(log_message=f"No metadata match for file {file}")
+
+  return metadata_dict
+
+def clear_directory(directory):
+    """
+    Clears all files and subdirectories in the given directory.
+
+    Parameters:
+    directory (str): The directory to be cleared.
+
+    Returns:
+    None
+    """
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        file_path = os.path.normpath(file_path)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+
+def resampler(audio_path, save_dir):
+    """
+    Resamples the given audio file to 16kHz and saves the output to the specified directory.
+
+    Parameters:
+    audio_path (str): The path to the original audio file.
+    save_dir (str): The directory where the resampled audio file will be saved.
+
+    Returns:
+    str: The path to the resampled audio file.
+    """
+    waveform, sample_rate = torchaudio.load(audio_path)
+
+    resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)
+
+    waveform_resampled = resampler(waveform)
+
+    # create new path with original filename
+    base_filename = os.path.basename(audio_path)
+    new_path = os.path.join(save_dir, base_filename)
+    new_path = os.path.normpath(new_path)
+    
+
+    # save the resampled audio
+    torchaudio.save(new_path, waveform_resampled, sample_rate=16000)
+
+    return new_path
+
+# Function to convert strero files to mono
+def stereo_to_mono(directory_path):
+  """
+    Converts all stereo audio files in the given directory to mono.
+
+    Parameters:
+    directory_path (str): The directory containing audio files to be converted.
+
+    Returns:
+    None
+    """
+  # Loop through all files in the directory
+  for filename in os.listdir(directory_path):
+    # Check if the file is a .wav file
+    if filename.endswith('.wav'):
+      # Get the full path of the file
+      file_path = os.path.join(directory_path, filename)
+      file_path = os.path.normpath(file_path)
+
+      # Load audio file
+      audio = AudioSegment.from_wav(file_path)
+
+      # If the audio file is stereo
+      if audio.channels == 2:
+        print(f"Converting stereo file: {filename}")
+
+        # Convert to mono
+        mono_audio = audio.set_channels(1)
+
+        # Replace the original file with the mono version
+        mono_audio.export(file_path, format='wav')
+
+def Preprocess_audio(input_dir, temp_file_storage, resampled_audio_dir, progress_callback):
+    """
+    Preprocesses audio files from input_dir, through temp_file_storage, and into resampled_audio_dir
+
+    This function performs several preprocessing steps on audio files:
+    - Splits audio files into smaller segments.
+    - Clears the output directory.
+    - Resamples audio files to a specified sample rate.
+    - Converts stereo audio files to mono.
+
+    Parameters:
+    input_dir (str): The directory containing the original audio files to be processed.
+    temp_file_storage (str): The directory where the processed audio segments will be saved.
+    resampled_audio_dir (str): The directory where resampled audio files will be saved.
+    progress_callback (function): A callback function for updating the progress of the preprocessing. This function should accept two parameters: a message (str) and an optional progress value (float or int).
+
+
+    Returns:
+    None
+    """
+    try:
+        
+        # Make sure temp_file_storage exists
+        if not os.path.exists(temp_file_storage):
+            os.makedirs(temp_file_storage)
+
+        # Clears the output directory
+        progress_callback("Preprocessing Step 1/5: Clearing temp_file_storage...", 5,"Preprocessing Step 1/5: Clearing temp_file_storage...")
+        clear_directory(temp_file_storage)
+        
+        # Splits and processes the audio files
+        progress_callback("Preprocessing Step 2/5: Splitting audio files...", 10,"Preprocessing Step 2/5: Splitting audio files...")
+        split_all_audio_files(input_dir, temp_file_storage, progress_callback)
+        
+        # Clear the ResampledAudio directory and resample the split audio files
+        if not os.path.exists(resampled_audio_dir):
+            os.makedirs(resampled_audio_dir)
+        
+        clear_directory(resampled_audio_dir)
+        progress_callback("Preprocessing Step 3/5: Clearing Resampled Audio...", 15,"Preprocessing Step 3/5: Clearing Resampled Audio...")
+        
+        # Convert to mono and resample the audio files
+        progress_callback("Preprocessing Step 4/5: Converting files to mono...", 20,"Preprocessing Step 4/5: Converting files to mono...")
+        stereo_to_mono(temp_file_storage)        
+        
+        audio_files = [f for f in os.listdir(temp_file_storage) if f.endswith('.wav') or f.endswith('.WAV')]
+        file_count = len(audio_files)
+        progress_callback(log_message= f"Preprocessing Step 5/5: Resampling audio files...")
+        for i, filename in enumerate(audio_files, start=1):
+            filepath = os.path.join(temp_file_storage, filename)
+            filepath = os.path.normpath(filepath)
+            resampler(filepath, resampled_audio_dir)
+            # Update progress based on number of files processed
+            progress_percentage = 20 + (i / file_count) * 50
+            progress_callback(f"Preprocessing Step 5/5: Resampling audio files...", progress_percentage)
+        
+        progress_callback("Finished Preprocessing", 100,"Finished Preprocessing")
+    except Exception as e:
+        messagebox.showerror('Error', f'An error occurred while preprocessing audio: {str(e)}')
+        progress_callback(log_message=f'An error occurred while preprocessing audio: {str(e)}')
---
+++ gui.py
@@ -0,0 +1,245 @@
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog, scrolledtext
+import os
+import threading
+import queue
+
+class RibbitRadarGUI:
+    """
+    A class to represent the GUI of RibbitRadar application.
+
+    Attributes:
+    root (tk.Tk): The main window of the application.
+    preprocess_audio_callback (function): A callback function for preprocessing audio.
+    """
+    
+    def __init__(self, root, resampled_audio_path):
+        """
+        Constructs all the necessary attributes for the RibbitRadarGUI object.
+
+        Parameters:
+        root (tk.Tk): The main window of the application.
+        resampled_audio_path (str): Path to the directory where resampled audio files will be saved.
+        """
+        self.root = root
+        self.resampled_audio_path = resampled_audio_path
+        self.inference_callback = None  # Initialize as None
+        self.create_widgets()
+        self.update_queue = queue.Queue()
+        self.check_queue()
+
+    def set_inference_callback(self, callback):
+        """
+        Sets the callback function for running inference. 
+
+        Parameters:
+        callback (function): The callback function to set, which will be called when "Run Inference" is clicked
+        """
+        self.inference_callback = callback
+
+    def check_queue(self):
+        try:
+            while not self.update_queue.empty():
+                message, value, log_message = self.update_queue.get_nowait()
+                # Handle GUI updates based on the message content
+                if message == "Enable Run Button":
+                    self.enable_run_button()
+                else:
+                    self.update_progress(message, value, log_message)
+        finally:
+            self.root.after(100, self.check_queue)
+
+    
+    @staticmethod
+    def create_splash_screen(root):
+        """
+        Creates a splash screen window.
+
+        Parameters:
+        root (tk.Tk): The root window of the application.
+
+        Returns:
+        tk.Toplevel: The splash screen window.
+        """
+        splash = tk.Toplevel(root)
+        splash.title("Loading")
+        splash.geometry("300x100")
+        tk.Label(splash, text="Starting RibbitRadar, please wait...").pack(pady=20)
+        splash.update()
+        return splash
+    
+    # In gui.py, inside the RibbitRadarGUI class
+    def enable_run_button(self):
+        self.run_button.config(state='normal')
+
+
+    def validate_paths(self):
+        # Normalize and trim paths
+        input_folder = os.path.normpath(self.input_folder_entry.get().strip())
+        output_folder = os.path.normpath(self.output_folder_entry.get().strip())
+        output_file = self.output_file_entry.get().strip()
+
+        # Validate Input Folder Path
+        input_folder = self.input_folder_entry.get()
+        if not os.path.isdir(input_folder):
+            messagebox.showerror('Invalid Path', 'The input folder path is invalid.')
+            return False
+
+        # Validate Output Folder Path
+        output_folder = self.output_folder_entry.get()
+        if not os.path.isdir(output_folder):
+            messagebox.showerror('Invalid Path', 'The output folder path is invalid.')
+            return False
+
+        # Validate Output File Name (ensure it's not empty)
+        output_file = self.output_file_entry.get()
+        if not output_file:
+            messagebox.showerror('Invalid Name', 'Please enter a valid output file name.')
+            return False
+        
+        if any(char in output_file for char in r'\/:*?"<>|'):
+            messagebox.showerror('Invalid File Name', 'The output file name contains invalid characters.')
+            return False
+
+        # Check for output file already exists
+        full_path = os.path.join(output_folder, output_file)
+        if os.path.exists(full_path):
+            messagebox.showerror('File Exists', 'A file with this output name already exists in the folder. Please choose a different name to avoid overwrite.')
+            return False
+        
+        return True
+
+
+    def create_widgets(self):
+        """
+        Creates and arranges the widgets in the GUI.
+        """
+        # Main buttons and progress updates
+        main_frame = ttk.Frame(self.root)
+        main_frame.grid(column=0, row=0, sticky='nwse')
+
+        # Instructions for using the application
+        instruction_frame = ttk.Frame(self.root)
+        instruction_frame.grid(column=1, row=0, sticky='n', padx=10)
+
+        # Input Folder
+        ttk.Label(main_frame, text='Input Folder:').grid(column=0, row=4, padx=10, pady=10)
+        self.input_folder_entry = ttk.Entry(main_frame)
+        self.input_folder_entry.grid(column=1, row=4, padx=10, pady=10)
+        input_folder_button = ttk.Button(main_frame, text="Browse", command=lambda: self.select_directory(self.input_folder_entry))  
+        input_folder_button.grid(column=2, row=4, padx=10, pady=10)  
+
+        # Output File Name
+        ttk.Label(main_frame, text='Unique Output File Name:').grid(column=0, row=2, padx=10, pady=10)
+        self.output_file_entry = ttk.Entry(main_frame)
+        self.output_file_entry.grid(column=1, row=2, padx=10, pady=10)
+
+        # Output Folder
+        ttk.Label(main_frame, text='Output Location:').grid(column=0, row=3, padx=10, pady=10)
+        self.output_folder_entry = ttk.Entry(main_frame)
+        self.output_folder_entry.grid(column=1, row=3, padx=10, pady=10)
+        base_path_button = ttk.Button(main_frame, text="Browse", command=lambda: self.select_directory(self.output_folder_entry))  
+        base_path_button.grid(column=2, row=3, padx=10, pady=10)  
+
+        # Run Button
+        self.run_button = ttk.Button(self.root, text='Run Inference', command=self.run_inference)
+        self.run_button.grid(column=0, row=6, columnspan=2, pady=20)
+
+        # Add a log text area with scrollbar
+        self.log_area = scrolledtext.ScrolledText(main_frame, height=10, state='disabled')
+        self.log_area.grid(column=0, row=9, columnspan=3, padx=10, pady=10)
+
+        # Add a status label
+        self.status_label = ttk.Label(main_frame, text="")
+        self.status_label.grid(column=0, row=7, columnspan=3)
+
+        # Add a progress bar
+        self.progress_bar = ttk.Progressbar(main_frame, orient="horizontal", length=300, mode="determinate")
+        self.progress_bar.grid(column=0, row=8, columnspan=3, pady=10)
+
+        # Instruction Panel
+        instruction_label = ttk.Label(instruction_frame, text="Instructions", font=("Helvetica", 16, "bold"))
+        instruction_label.grid(column=0, row=0, sticky='nw', pady=(0, 10))
+
+        instruction_text = tk.Text(instruction_frame, height=25, width=50, wrap="word", state='disabled', bg=self.root.cget('bg'), relief='flat')
+        instruction_text.grid(column=0, row=1, sticky='nw')
+
+        # Adding instructions
+        instructions = """
+        How to Use RibbitRadar:
+        ------------------------
+        1. Select the Input Folder containing your audio files.
+        2. Enter a unique name for the Output File.
+        3. Choose the Output Location to save the results.
+        4. Click 'Run Inference' to start processing.
+
+        Note: The Output File name must be unique, if there is already a file with that name in that location
+          it will not be saved
+        """
+        instruction_text.config(state='normal')
+        instruction_text.insert('end', instructions)
+        instruction_text.config(state='disabled')
+
+        # Making the instruction text read-only and scrollable
+        instruction_text_scrollbar = ttk.Scrollbar(instruction_frame, orient='vertical', command=instruction_text.yview)
+        instruction_text_scrollbar.grid(column=1, row=1, sticky='ns')
+        instruction_text['yscrollcommand'] = instruction_text_scrollbar.set
+
+    def update_log(self, message):
+        """Update the log area with new messages."""
+        self.log_area.config(state='normal')  # Enable editing of the text area
+        self.log_area.insert(tk.END, message + '\n')  # Append message
+        self.log_area.yview(tk.END)  # Auto-scroll to the bottom
+        self.log_area.config(state='disabled')  # Disable editing of the text area
+
+    def update_progress(self, message=None, value=None, log_message=None):
+        """Update the progress bar and optionally the status label and log message."""
+        if message is not None:
+            self.status_label.config(text=message)
+        if value is not None:
+            self.progress_bar['value'] = value
+        if log_message is not None:
+            self.update_log(log_message)
+        self.root.update_idletasks()  # Update the GUI
+
+
+    def select_directory(self, entry_widget):
+        """
+        Opens a dialog to select a directory and updates the entry widget with the chosen path.
+
+        Parameters:
+        entry_widget (ttk.Entry): The entry widget to update with the selected directory.
+        """
+        selected_directory = filedialog.askdirectory()
+        entry_widget.delete(0, tk.END)
+        entry_widget.insert(0, selected_directory)
+
+    def run_inference(self):
+        """
+        Triggers the inference process using the provided callback in a separate thread.
+        """
+        if not self.validate_paths():
+            return  # Stop the process if paths are invalid
+        if self.inference_callback is not None:
+            try:
+                # Disable the Run button to prevent multiple clicks
+                self.run_button.config(state='disabled')
+                # Run the inference in a separate thread
+                inference_thread = threading.Thread(target=lambda: self.inference_callback())
+                inference_thread.start()
+            except Exception as e:
+                messagebox.showerror('Error', f'An error occurred while running inference: {str(e)}')
+                # Ensure the run button is enabled in case of error
+                self.run_button.config(state='normal')
+        else:
+            messagebox.showerror('Error', 'Inference callback is not set.')
+            self.run_button.config(state='normal')  # Ensure the button is re-enabled
+
+    def set_inference_callback(self, callback):
+        """
+        Sets the callback function for running inference.
+
+        Parameters:
+        callback (function): The callback function to set.
+        """
+        self.inference_callback = callback
